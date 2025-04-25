cfc8ae51e6c4abb1a98487dd3ff41f8ad5fbe11b
Sami Al-Dury <git@aldury.com>
2025-04-25 10:09:29 +0200

    args, opts

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,85 @@
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import fs from 'node:fs'
+import { parseArgs, styleText, type ParseArgsOptionDescriptor } from 'node:util'
+import debug from 'debug'
 import sharp from 'sharp'
 import { filesize } from 'filesize'
-import { parseArgs } from 'node:util'
-import debug from 'debug'
 
 const d = debug('image-compressor:main')
 
-const args = parseArgs({
-	options: {
-		input: {
-			type: 'string',
-			short: 'i',
-			default: 'input',
-			help: 'Input directory',
-		},
-		output: {
-			type: 'string',
-			short: 'o',
-			default: 'output',
-			help: 'Output directory',
-		},
-		quality: {
-			type: 'string',
-			short: 'q',
-			default: '50',
-			help: 'Quality of the output image (0-100)',
-		},
-	},
-})
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+const base = path.join(dirname, '..')
+
+const ogArgs = process.argv.slice(2)
+
+interface CLIOptions {
+	[longOption: string]: ParseArgsOptionDescriptor & { help: string }
+}
 
 const options = {
-	...args.values,
+	input: {
+		type: 'string',
+		short: 'i',
+		default: path.join(base, 'input'),
+		help: 'Input directory',
+	},
+	output: {
+		type: 'string',
+		short: 'o',
+		default: path.join(base, 'output'),
+		help: 'Output directory',
+	},
+	quality: {
+		type: 'string',
+		short: 'q',
+		default: '50',
+		help: 'Quality of the output image (0-100)',
+	},
+} satisfies CLIOptions
+
+function help() {
+	let out = `image-compressor [ARGS?] [...OPTS?]\n`
+
+	out += `\nargs:
+help
+\tShow help.`
+
+	out += `\n\noptions:`
+	for (const [k, o] of Object.entries(options)) {
+		out += `
+--${k} (-${o.short}):
+\t${o.help}.
+\tDefault: ${o.default}\n`
+	}
+
+	return out
+}
+
+if (ogArgs[0] === 'help') {
+	console.log(help())
+	process.exit(0)
+}
+
+const args = parseArgs({
+	options,
+	args: ogArgs,
+})
+
+const opts = {
+	input: path.resolve(args.values.input),
+	ouput: path.resolve(args.values.output),
 	quality: parseInt(args.values.quality, 10),
 } as const
 
-d('args: %o', args.values)
-d('options: %o', options)
+d('args: %O', args.values)
+d('opts: %O', opts)
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
-
-const base = path.join(dirname, '..')
-const input = path.join(base, 'input')
-const output = path.join(base, 'output')
+if (!fs.existsSync(opts.input)) {
+	console.error(styleText('red', `${opts.input} does not exist.`))
+	process.exit(1)
+}
 
 // @ts-expect-error
 async function* walk(dir: string) {
@@ -56,11 +91,12 @@ async function* walk(dir: string) {
 }
 
 const OUTPUT_EXTENSION = 'avif'
-const SUPPORTED_EXTENSIONS = ['png', 'jpg', 'webp'] as const
+const SUPPORTED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'webp'] as const
 const supportedExtensions = new Set(SUPPORTED_EXTENSIONS)
 type SupportedExtension = (typeof SUPPORTED_EXTENSIONS)[number]
 
 const files = [] as {
+	id: string
 	type: SupportedExtension
 	path: string
 	originalSize: number
@@ -70,31 +106,43 @@ const files = [] as {
 	formattedReduction?: string
 }[]
 
-for await (const p of walk(input)) {
+for await (const p of walk(opts.input)) {
 	const ext = path.extname(p).split('.').at(1)
 
 	if (!ext) continue
-	if (!supportedExtensions.has(ext as never)) continue
+	// if (!supportedExtensions.has(ext as never)) continue
 
 	const stat = await fs.promises.stat(p)
 
 	files.push({
+		id: crypto.randomUUID(),
 		type: ext as SupportedExtension,
 		path: p,
 		originalSize: stat.size,
 	})
 }
 
+d('collected files: %O', files)
+
 for (const f of files) {
-	const outputPath = f.path
-		.replace('input', 'output')
-		.replace('.' + f.type, '.' + OUTPUT_EXTENSION)
+	const b = debug('image-compressor:' + f.id)
+	let outputPath = f.path.replace('input', 'output')
 	const outputDir = path.dirname(outputPath)
 
 	if (!fs.existsSync(outputDir)) {
 		fs.mkdirSync(outputDir, { recursive: true })
 	}
 
+	// Copy the file as is, if not supported
+	if (!supportedExtensions.has(f.type)) {
+		b('unsuported file %s, copying as is', f.path)
+		await fs.promises.copyFile(f.path, outputPath)
+		continue
+	}
+
+	outputPath = outputPath.replace('.' + f.type, '.' + OUTPUT_EXTENSION)
+
+	b('converting and compressing %s', f.path)
 	const info = await new Promise<sharp.OutputInfo>((res, rej) => {
 		sharp(f.path)
 			.avif({ quality: 50 })
@@ -103,13 +151,14 @@ for (const f of files) {
 				else res(info)
 			})
 	})
+	b('%s converted and compressed', f.path)
+
+	const idx = files.findIndex((file) => file.id === f.id)!
+	const el = files[idx]!
 
 	const newSize = info.size
-	const index = files.findIndex((file) => file.path === f.path)!
-	const el = files[index]!
-	el.newSize = newSize
-	el.reduction = el.originalSize - newSize
 	const decrease = el.originalSize - newSize
+	el.newSize = newSize
 	el.reduction = decrease
 	el.reductionPercent = Math.round((decrease / el.originalSize) * 100)
 	el.formattedReduction = filesize(el.reduction)
